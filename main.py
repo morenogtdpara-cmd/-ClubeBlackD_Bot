0deb67385caba25871ca0ba3efa7780b8275cdfa
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-17 10:03:46 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ async def receber_album(
     update: Update,
     context: ContextTypes.DEFAULT_TYPE
 ):
-
+    print("ALBUM RECEBEU MIDIA")
     if update.effective_user.id != OWNER_ID:
 
         return
