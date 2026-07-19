3f0d80d9e31792ca8c3ee849ef45ea3b73f58574
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-19 17:44:53 -0300

    Atualizar o keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -40,15 +40,7 @@ def album_keyboard():
             InlineKeyboardButton(
                 "📤 ENVIAR ÁLBUM",
                 callback_data="album_agora"
-            )
-        ],
-        [
-            InlineKeyboardButton(
-                "✅ FINALIZAR ÁLBUM",
-                callback_data="finalizar_album"
-            )
-        ],
-        [
+            ),
             InlineKeyboardButton(
                 "🔙 VOLTAR",
                 callback_data="voltar"
