c86b574635aeea73a2a4eef8f861f5146b7ecac6
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-19 15:30:43 -0300

    Atualizar o database.py

--- database.py
+++ database.py
@@ -1,1 +1,40 @@
-1
+import sqlite3
+from config import DB_PATH
+
+
+def conectar():
+    return sqlite3.connect(DB_PATH)
+
+
+def init_db():
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id INTEGER PRIMARY KEY,
+            nome TEXT,
+            username TEXT
+        )
+    """)
+
+    conn.commit()
+    conn.close()
+
+
+def salvar_usuario(user_id, nome, username):
+    conn = conectar()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        INSERT OR REPLACE INTO usuarios
+        (id, nome, username)
+        VALUES (?, ?, ?)
+    """, (
+        user_id,
+        nome,
+        username
+    ))
+
+    conn.commit()
+    conn.close()
