334864ae56930c8a1ce4ba8c7ce04b8107858e3f
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-19 02:37:05 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def main():
     app = Application.builder().token(BOT_TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
-    app.add_handler(CommandHandler("painel", painel))
+    app.add_handler(CommandHandler("manager", manager))
 
     app.add_handler(
         CallbackQueryHandler(callbacks)
