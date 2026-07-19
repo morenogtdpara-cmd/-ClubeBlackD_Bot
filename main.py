18f24f777fe14a829a516d46a40d2b99c2e09f7b
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-19 15:33:20 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,1 +1,60 @@
-1
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler
+)
+
+from config import BOT_TOKEN
+from database import init_db
+from handlers import (
+    start,
+    manager,
+    callbacks
+)
+
+from scheduler import iniciar_scheduler
+
+
+def main():
+
+    init_db()
+
+    app = Application.builder().token(
+        BOT_TOKEN
+    ).build()
+
+
+    app.add_handler(
+        CommandHandler(
+            "start",
+            start
+        )
+    )
+
+
+    app.add_handler(
+        CommandHandler(
+            "manager",
+            manager
+        )
+    )
+
+
+    app.add_handler(
+        CallbackQueryHandler(
+            callbacks
+        )
+    )
+
+
+    iniciar_scheduler(app)
+
+
+    print("BOT ONLINE")
+
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
