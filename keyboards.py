115621d919911863fbfad54683618753e0a201b7
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-19 15:31:12 -0300

    Atualizar o keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,1 +1,45 @@
-1
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def painel_keyboard():
+
+    return InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(
+                "📢 DIVULGAR",
+                callback_data="divulgar"
+            ),
+            InlineKeyboardButton(
+                "🖼️ ÁLBUM",
+                callback_data="album"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "⭐ FEEDBACKS",
+                callback_data="feedbacks"
+            ),
+            InlineKeyboardButton(
+                "📊 RELATÓRIO",
+                callback_data="relatorio"
+            )
+        ],
+        [
+            InlineKeyboardButton(
+                "⏰ FILA",
+                callback_data="fila"
+            )
+        ]
+    ])
+
+
+def vip_keyboard(link):
+
+    return InlineKeyboardMarkup([
+        [
+            InlineKeyboardButton(
+                "🔥 ENTRAR NO VIP 🔥",
+                url=link
+            )
+        ]
+    ])
