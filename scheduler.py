349746b23023e26315d6c0e6e4690b25df9f0134
morenogtdpara-cmd <morenogtdpara@gmail.com>
2026-07-20 19:58:52 -0300

    Atualizar o scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,12 @@
 from telegram.ext import Application
 
+from fila import pegar_fila
+
 
 def iniciar_scheduler(app: Application):
 
-    # Futuramente:
-    # - agendamento de divulgação
-    # - agendamento de álbum
-    # - fila de publicações
+    fila = pegar_fila()
 
-    pass
+    print(
+        f"SCHEDULER ONLINE | Itens na fila: {len(fila)}"
+    )
